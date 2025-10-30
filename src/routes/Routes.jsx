8959f9d2c8323754802712eae51fd7e2c7424bf3
Aion <aionshahriar23@gmail.com>
2025-10-30 18:14:47 +0600

    Folder structure, Router setup and others

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,38 @@
+import { createBrowserRouter } from "react-router";
+import MainLayout from "../layOuts/MainLayout";
+import ErrorPage from "../pages/ErrorPage";
+import HomePage from "../pages/HomePage";
+import AllAppsPage from "../pages/AllAppsPage";
+import AppDetailsPage from "../pages/AppDetailsPage";
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout></MainLayout>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+        {
+            index:true,
+            element: <HomePage></HomePage>
+        },
+        {
+            path: '/allapps',
+            element: <AllAppsPage></AllAppsPage>
+        },
+        
+        {
+            path: '/app/:id',
+            Component: <AppDetailsPage></AppDetailsPage>
+        }
+        
+    ]
+  },
+  
+  
+  
+])
+
+export default router;

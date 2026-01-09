388afa3d5947d0b2f4bbfd2308e33d8ee2a4afd4
Aion <aionshahriar23@gmail.com>
2026-01-09 19:08:50 +0600

    App details and error route page design

--- src/pages/AppDetailsPage.jsx
+++ src/pages/AppDetailsPage.jsx
@@ -19,53 +19,59 @@ const AppDetailsPage = () => {
 
     const {image, title, companyName, description, size, reviews, ratingAvg, downloads} = app;
     return (
-        <div className="card bg-base-100 border shadow-sm">
+        <div className="card bg-base-100 shadow-sm p-5">
 
-            <div>
+            <div className='mt-10 flex gap-14'>
+                <div className=''>
                 <figure>
                     <img src={image} alt="app image" />
                 </figure>
             </div>
 
-            <div>
-                <div>
-                    <h2 className='text-3xl'>{title}</h2>
+            <div className='max-w-full'>
+                <div className='mb-3'>
+                    <h2 className='text-3xl font-semibold'>{title}</h2>
                     <p>Developed by <span className='primary'>{companyName}</span></p>
                 </div>
+                <hr />
 
-                <div>
+                <div className='mt-4 grid grid-cols-1 md:grid-cols-3 gap-5'>
                     <div>
                         <FaDownload className='text-green-400'/>
                         <p>Downloads</p>
-                        <p>{downloads}</p>
+                        <p className='text-4xl font-bold'>{downloads}</p>
                     </div>
 
                     <div>
                         <img src={ratingImg} alt="" className='w-4'/>
                         <p>Average Ratings</p>
-                        <p>{ratingAvg}</p>
+                        <p className='text-3xl font-bold'>{ratingAvg}</p>
                     </div>
 
                     <div>
                         <img src={likeImg} alt="" className='w-4'/>
                         <p>Total Reviews</p>
-                        <p>{reviews}</p>
+                        <p className='text-3xl font-bold'>{reviews}</p>
                     </div>
 
                 </div>
 
-                <div>
-                    <Link to="/installed"><button onClick={() => updateList(app)} className="btn btn-primary bg bg-green-300">Install Now<span>({size})</span></button></Link>
+                <div className='mt-5 ml-5'>
+                    <Link to="/installed"><button onClick={() => updateList(app)} className="btn bg-green-400 text-white">Install Now<span>({size} MB)</span></button></Link>
                 </div>
             </div>
-
-            <div className='ratingsGraph'>
-
             </div>
 
-            <div>
-                <h2>Description</h2>
-                <p>{description}</p>
+            <div className='ratingsGraph mt-10'>
+                <h2 className='text-2xl font-semibold'>Ratings</h2>
+            </div>
+
+            <div className='mt-10'>
+                <h2 className='text-2xl font-semibold'>Description</h2>
+                <p className='whitespace-pre-line mt-4'>{description}</p>
+
+                
+
             </div>
             
             
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import { useRouteError } from 'react-router';
+// import { useRouteError } from 'react-router';
+
+import errorImg from '../assets/error-404.png'
 
 
 const ErrorPage = () => {
-    const error = useRouteError();
+    // const error = useRouteError();
     return (
-        <div>
-            <h3>404, Not found!!!!</h3>
-            <p>Problem : {error.message}</p>
-            <img src="" alt="img" />
+        <div className='w-100 mx-auto'>
+            <img src={errorImg} alt="error-img" className='mt-24' />
+            <h2 className='text-4xl font-semibold mt-10'>Oops, page not found!</h2>
+            <p className='text-xl mt-4'>The page you are looking for is not available.</p>
+            <button className='btn bg text-white mt-4 ml-36'>Go Back!</button>
+            
         </div>
     );
 };
